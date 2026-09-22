ce273a46f38b8779c42461049724e0ab4db5882b
arthur-rodrigues <arthurodrigues4433@gmail.com>
2026-09-22 07:52:09 -0300

    chore: inicializa projeto Cestto

---
+++ src/main/java/com/cestto/cestto/CesttoApplication.java
@@ -0,0 +1,13 @@
+package com.cestto.cestto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CesttoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CesttoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cestto/cestto/CesttoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cestto.cestto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CesttoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
